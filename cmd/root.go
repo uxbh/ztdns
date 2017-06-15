15f3a903f5ebf445b5825495b0d2aa7fd054aae6
Aaryn Smith <unixblackhole@gmail.com>
2017-06-15 11:13:35 -0500

    Remove extreneous example code

--- cmd/root.go
+++ cmd/root.go
@@ -34,14 +34,8 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports Persistent Flags, which, if defined here,
-	// will be global for your application.
-
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ztdns.yaml)")
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
 }
 
 // initConfig reads in config file and ENV variables if set.
