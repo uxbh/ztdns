f20a8220dd4422809afe339371abdcc6c5b63a32
Aaryn Smith <unixblackhole@gmail.com>
2017-06-15 11:13:49 -0500

    Add mkconfig command

---
+++ cmd/mkconfig.go
@@ -0,0 +1,52 @@
+// Copyright © 2017 uxbh
+// This file is part of gitlab.com/uxbh/ztdns.
+
+package cmd
+
+import (
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// mkconfigCmd represents the mkconfig command
+var mkconfigCmd = &cobra.Command{
+	Use:   "mkconfig",
+	Short: "Make a new config file",
+	Long: `mkconfig (ztdns mkconfig) creates a new configuation file.
+If you do not specify a filename the default is ./.ztdns.toml
+
+Example: ztdns mkconfig .filenames.toml`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Work your own magic here
+		filename := "./.ztdns.new.toml"
+		if len(args) > 0 {
+			filename = args[0]
+		}
+		log.Printf("Creating new config file in %s", filename)
+		file, err := os.Create(filename)
+		if err != nil {
+			log.Fatalf("Could not create file: %s", err.Error())
+		}
+		defer file.Close()
+		file.WriteString(`# Configuration file for ztDNS
+
+suffix = "zt"
+port = 53
+
+# This section contains information related to your ZeroTier config
+[ZT]
+# API is used to contact the ZeroTier controller API service.
+API = ""
+# Network is your ZeroTier network ID.
+Network = ""
+# URL is the url of the ZeroTier controller API
+URL = "https://my.zerotier.com/api"
+`)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(mkconfigCmd)
+}
